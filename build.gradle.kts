bd60c19df4fc40bf90ee3c40868df443c98a24cf
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-20 17:55:02 +0800

    Change TheOneProbe dependency from implementation to compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -122,7 +122,7 @@ dependencies {
     compileOnly(rfg.deobf("cofh:ThermalExpansion:1.12.2-5.5.4.43:universal"))
     compileOnly(rfg.deobf("cofh:RedstoneFlux:1.12-2.1.0.7:universal"))
     compileOnly(rfg.deobf("codechicken:CodeChickenLib:1.12.2-3.2.3.358:universal"))
-    implementation(rfg.deobf("mcjty.theoneprobe:TheOneProbe-1.12:1.12-1.4.28-17"))
+    compileOnly(rfg.deobf("mcjty.theoneprobe:TheOneProbe-1.12:1.12-1.4.28-17"))
     curseMaven("oe", 840576, 4670168, runtime = true)
 }
 
