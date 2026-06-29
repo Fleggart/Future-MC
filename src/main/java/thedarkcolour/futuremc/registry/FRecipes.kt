88d89f90e7e5e567724a3574326bb7d4d025bd91
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-30 06:15:19 +0800

    Update FRecipes.kt

--- src/main/java/thedarkcolour/futuremc/registry/FRecipes.kt
+++ src/main/java/thedarkcolour/futuremc/registry/FRecipes.kt
@@ -2,12 +2,10 @@ package thedarkcolour.futuremc.registry
 
 import net.minecraft.init.Blocks
 import net.minecraft.init.Items
-import net.minecraft.init.MobEffects
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.IRecipe
 import net.minecraft.item.crafting.Ingredient
 import net.minecraft.item.crafting.ShapedRecipes
-import net.minecraft.potion.PotionEffect
 import net.minecraft.util.NonNullList
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.registry.GameRegistry
@@ -20,7 +18,6 @@ import thedarkcolour.futuremc.compat.checkHarvestCraft
 import thedarkcolour.futuremc.compat.checkPlants
 import thedarkcolour.futuremc.compat.checkTConstruct
 import thedarkcolour.futuremc.config.FConfig
-import thedarkcolour.futuremc.item.SuspiciousStewItem
 import thedarkcolour.futuremc.recipe.crafting.TrapdoorRecipe
 
 /**
@@ -31,24 +28,7 @@ import thedarkcolour.futuremc.recipe.crafting.TrapdoorRecipe
  */
 object FRecipes {
     fun registerRecipes(recipes: IForgeRegistryModifiable<IRecipe>) {
-        if (FConfig.villageAndPillage.suspiciousStew) {
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 8), PotionEffect(MobEffects.REGENERATION, 140, 1))
-            if (FConfig.villageAndPillage.cornflower.enabled)
-                SuspiciousStewItem.addRecipe(recipes, ItemStack(FBlocks.CORNFLOWER), PotionEffect(MobEffects.JUMP_BOOST, 100, 1))
-            if (FConfig.villageAndPillage.lilyOfTheValley.enabled)
-                SuspiciousStewItem.addRecipe(recipes, ItemStack(FBlocks.LILY_OF_THE_VALLEY), PotionEffect(MobEffects.POISON, 220, 1))
-            if (FConfig.villageAndPillage.witherRose.enabled)
-                SuspiciousStewItem.addRecipe(recipes, ItemStack(FBlocks.WITHER_ROSE), PotionEffect(MobEffects.WITHER, 140, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 4), PotionEffect(MobEffects.WEAKNESS, 160, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 5), PotionEffect(MobEffects.WEAKNESS, 160, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 6), PotionEffect(MobEffects.WEAKNESS, 160, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 7), PotionEffect(MobEffects.WEAKNESS, 160, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 3), PotionEffect(MobEffects.BLINDNESS, 140, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 2), PotionEffect(MobEffects.FIRE_RESISTANCE, 60, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER), PotionEffect(MobEffects.NIGHT_VISION, 100, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.RED_FLOWER, 1, 1), PotionEffect(MobEffects.SATURATION, 100, 1))
-            SuspiciousStewItem.addRecipe(recipes, ItemStack(Blocks.YELLOW_FLOWER), PotionEffect(MobEffects.SATURATION, 100, 1))
-        }
+        // SuspiciousStewItem recipes removed
 
         if (FConfig.villageAndPillage.smoothStone)
             GameRegistry.addSmelting(ItemStack(Blocks.STONE), ItemStack(FBlocks.SMOOTH_STONE), 0.1f)
