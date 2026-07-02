c9aa5562ae02915544c87aa6ffb628ede46528e9
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-02 17:26:05 +0900

    Delete src/main/java/thedarkcolour/futuremc/client/gui/GuiCartographyTable.kt

--- src/main/java/thedarkcolour/futuremc/client/gui/GuiCartographyTable.kt
+++
@@ -1,8 +0,0 @@
-package thedarkcolour.futuremc.client.gui
-
-import thedarkcolour.futuremc.container.ContainerCartographyTable
-
-class GuiCartographyTable(container: ContainerCartographyTable) : FGui<ContainerCartographyTable>(container) {
-    override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) {
-    }
-}
